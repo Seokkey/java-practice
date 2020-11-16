6767807307e4d4c72c4b8436367ac34ecae059cd
yooseok <yooseok@ust21.co.kr>
2020-11-16 20:36:09 +0900

    initial commit

---
+++ src/java005_ProgrammingBasic/ProgrammingBasic.java
@@ -0,0 +1,38 @@
+package java005_ProgrammingBasic;
+
+/**
+ * 입문 005.자바 프로그래밍의 기본
+ *
+ * Study Goal : 조건문 반복문을 이용한 프로그래밍 흐름의 이해
+ */
+
+public class ProgrammingBasic {
+
+    //1. 프로그램 실행 순서
+    // 프로그램은 위에서 아래로 순차적(Sequence)으로 실행된다.
+    // 프로그램의 순서를 제어하기 위함 조건문에는 if, switch, 삼항연산자(?:)가 있다.
+    // 프로그램의 특정 부분을 반복시키는 반복문에는 for, while, do~while이 있다.
+    // 조건문과 반복문을 이용해 문제를 해결하는 것을 "알고리즘" 이라고 한다.
+
+    //2. 주석
+    // 한줄 주석
+
+    /*
+     * 여러 줄 주석
+     */
+
+    //3. 조건문
+    // 참, 거짓에 따라 if ~ else
+    // 정수값에 따라 switch ~ case
+    // 간단한 if~else 구문은 삼항 연산자
+
+    int temp = 99;
+//    if (temp != 5) {
+//        System.out.println("dd");
+//    }
+//    if(temp%2 == 1) {
+//        temp = temp*3+1;
+//    } else {
+//        temp = temp/2;
+//    }
+}
